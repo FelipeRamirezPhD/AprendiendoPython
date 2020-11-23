1be652852a128572bb77fd9426273585ea685fa1
Felipe Ramírez, PhD <53548173+FelipeRamirezPhD@users.noreply.github.com>
2020-11-23 17:39:46 -0600

    Delete Nombre.py

--- Nombre.py
+++
@@ -1,8 +0,0 @@
-nombre=input("Nombre:")
-apellidos=input("Apellidos:")
-# Se concatenan los valores str, junto con la literal " "
-nombreCompleto=nombre+" "+apellidos
-# Se asigna a la variable la representación en mayúsculas
-# de lo que contenía.
-nombreCompleto=nombreCompleto.upper()
-print(nombreCompleto)
