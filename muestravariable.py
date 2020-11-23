d69f1b52e0bd059a27987d59c39ec48e220671d4
Felipe Ramírez, PhD <53548173+FelipeRamirezPhD@users.noreply.github.com>
2020-11-23 16:21:06 -0600

    Delete muestravariable.py

--- muestravariable.py
+++
@@ -1,8 +0,0 @@
-# Declara una variable, y la muestra.
-
-def main():
-  # En python se declara una variable por asignacion. El datatype depende
-  # del valor que se asigna.
-  txt="Hola mundo, con variable"
-  print(txt)
-  
